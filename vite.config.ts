2c9024b6864bb78d239292fe0fa40d4b51396cac
10xdrink <ittenex@gmail.com>
2026-03-08 17:30:59 +0530

    frontend v1111112222222222hjfsdjfjksdfsjkdftfgyuhgyuhchjkklllkddd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['chrome >= 64', 'edge >= 79', 'firefox >= 67', 'safari >= 12'],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
